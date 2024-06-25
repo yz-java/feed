4706c0b812255f812e44345871ade5479f8cb005
Kevin Jilissen <info@kevinjilissen.nl>
2024-06-25 08:54:24 +0200

    prometheus-node-exporter-lua: Align ethtool names
    Make sure the the ethtool metric names align with the names defined in
    the upstream prometheus-node-exporter project.
    
    As closely as possible, the behaviour of the implementation at
    https://github.com/prometheus/node_exporter/blob/400c3979931613db930ea035f39ce7b377cdbb5b/collector/ethtool_linux.go#L208
    is replicated.
    
    Signed-off-by: Kevin Jilissen <info@kevinjilissen.nl>

--- utils/prometheus-node-exporter-lua/files/usr/lib/lua/prometheus-collectors/ethtool-stats.lua
+++ utils/prometheus-node-exporter-lua/files/usr/lib/lua/prometheus-collectors/ethtool-stats.lua
@@ -1,7 +1,32 @@
 local ethtool = require "ethtool"
 
 local pattern_device = "^%s*([^%s:]+):"
-local pattern_metric = "^([a-zA-Z0-9:_]+)$"
+local pattern_metric = "_*[^0-9A-Za-z_]+_*"
+
+local metric_fqnames = {
+  rx_bytes = "node_ethtool_received_bytes_total",
+  rx_dropped = "node_ethtool_received_dropped_total",
+  rx_errors = "node_ethtool_received_errors_total",
+  rx_packets = "node_ethtool_received_packets_total",
+  tx_bytes = "node_ethtool_transmitted_bytes_total",
+  tx_errors = "node_ethtool_transmitted_errors_total",
+  tx_packets = "node_ethtool_transmitted_packets_total",
+  -- link info
+  supported_port = "node_network_supported_port_info",
+  supported_speed = "node_network_supported_speed_bytes",
+  supported_autonegotiate = "node_network_autonegotiate_supported",
+  supported_pause = "node_network_pause_supported",
+  supported_asymmetricpause = "node_network_asymmetricpause_supported",
+  advertised_speed = "node_network_advertised_speed_bytes",
+  advertised_autonegotiate = "node_network_autonegotiate_advertised",
+  advertised_pause = "node_network_pause_advertised",
+  advertised_asymmetricpause = "node_network_asymmetricpause_advertised",
+  autonegotiate = "node_network_autonegotiate",
+}
+local metric_replacement_words = {
+  rx = "received",
+  tx = "transmitted",
+}
 
 local function get_devices()
   local devices = {}
@@ -14,6 +39,23 @@ local function get_devices()
   return devices
 end
 
+local function replace_words(metric)
+  return string.gsub(metric, "(%a+)", function(word)
+    local replacement = metric_replacement_words[word]
+    if replacement then
+      return replacement
+    end
+    return word
+  end)
+end
+
+local function build_ethtool_fqname(metric)
+  local metricName = metric:gsub(pattern_metric, "_")
+  metricName = metricName:lower():gsub("^[%s_]+", "")
+  metricName = replace_words(metricName)
+  return "node_ethtool_" .. metricName
+end
+
 local function scrape()
   local eth = ethtool.open()
   local metrics = {}
@@ -21,14 +63,17 @@ local function scrape()
     local stats = eth:statistics(dev)
     if stats then
       for m_name, m_value in pairs(stats) do
-        if string.match(m_name, pattern_metric) then
-          local m = metrics[m_name]
-          if m == nil then
-            m = metric("ethtool_" .. m_name, "counter")
-            metrics[m_name] = m
-          end
-          m({device=dev}, m_value)
+        fqname = metric_fqnames[m_name]
+        if fqname == nil then
+          fqname = build_ethtool_fqname(m_name)
+          metric_fqnames[m_name] = fqname
         end
+        local m = metrics[fqname]
+        if m == nil then
+          m = metric(fqname, "counter")
+          metrics[fqname] = m
+        end
+        m({ device = dev }, m_value)
       end
     end
   end
